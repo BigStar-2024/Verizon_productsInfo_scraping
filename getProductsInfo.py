1a575a162883a8da2eaf004d26e0a70e4307d454
watanabe <kawanoaiyuki@gmail.com>
2024-05-01 04:44:38 +0900

    update

--- getProductsInfo.py
+++ getProductsInfo.py
@@ -1,10 +1,12 @@
 import csv
-import requests
+import requests 
+from selenium import webdriver
+import time
 
 #Read product IDs from the CSV file
 with open('product_ids.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
-    next(reader)  #Skip the header row
+    # next(reader)  #Skip the header row
     for row in reader:
         product_id = row[0]
         url = f"https://opendevelopment.verizonwireless.com/device-showcase/device/{product_id}"
@@ -17,3 +19,16 @@ with open('product_ids.csv', 'r', encoding='utf-8') as file:
             print(f"Successfully visited URL: {url}")
         else:
             print(f"Failed to visit URL: {url}")
+
+        #initialize the Chrome driver
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+
+        # head to product page
+        driver.get(url)
+
+        time.sleep(10)
+
+        
+        #Close the browser window
+        driver.quit()
