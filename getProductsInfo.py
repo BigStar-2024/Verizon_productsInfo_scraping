35775693e44ca8fd6cc13c89619db8ed97bc061e
watanabe <kawanoaiyuki@gmail.com>
2024-05-02 01:11:37 +0900

    update to overview

--- getProductsInfo.py
+++ getProductsInfo.py
@@ -17,7 +17,7 @@ if os.path.exists('Products_info.csv'):
 with open('Products_info.csv', 'a', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
 
-    writer.writerow(['Device Subtitle', 'Device Title'])
+    writer.writerow(['Device Subtitle', 'Device Title', 'Key Feature_1', 'Key Feature_2', 'Key Feature_3', 'Key Feature_4', 'Key Feature_5', 'Key Feature_6', 'Product description', 'Device Type', 'Network Technology', 'LTE catagory support'])
 
 with open('product_ids.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file) 
@@ -49,10 +49,31 @@ with open('product_ids.csv', 'r', encoding='utf-8') as file:
             device_title = device_title_element[1].text
             print(device_title)
 
+            key_feature_elements = driver.find_elements(By.CLASS_NAME, "details__content-item")
+            key_features = [feature.text for feature in key_feature_elements]
+
+            # Pad the key_features list with empty strings if there are fewer than 6 features
+            while len(key_features) < 6:
+                key_features.append('')
+            print(key_features)
+
+            product_description_element = driver.find_element(By.CLASS_NAME, "details__content-copy")
+            product_description = [product_description_element.text]
+            print(product_description)
+
+            overview_elements = driver.find_elements(By.CLASS_NAME, "definition-list-description")
+            overview_device_type = [overview_elements[0].text]
+            print(overview_device_type)
+            overview_network_technology = [overview_elements[1].text]
+            print(overview_network_technology)
+            overview_categor_support = [overview_elements[2].text]
+            print(overview_categor_support)
+            
+
             # Save the extracted information to a CSV file
-            with open('Products_info.csv', 'a', newline='') as file:
+            with open('Products_info.csv', 'a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
-                writer.writerow([device_subtitle, device_title])
+                writer.writerow([device_subtitle, device_title] + key_features + product_description + overview_device_type + overview_network_technology + overview_categor_support)
         except NoSuchElementException:
             print(f"Could not find device subtitle or title for {url}")
 
