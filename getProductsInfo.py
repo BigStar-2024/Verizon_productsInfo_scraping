ac3eed7d884a5a0a3590b3769d367a3d05d48d12
watanabe <kawanoaiyuki@gmail.com>
2024-05-01 22:56:11 +0900

    okay. subtitle scrap

--- getProductsInfo.py
+++ getProductsInfo.py
@@ -1,34 +1,47 @@
 import csv
-import requests 
+import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
-#Read product IDs from the CSV file
 with open('product_ids.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
-    # next(reader)  #Skip the header row
+    header_written = False
     for row in reader:
         product_id = row[0]
         url = f"https://opendevelopment.verizonwireless.com/device-showcase/device/{product_id}"
 
-        #Send an HTTP GET request to the URL
-        response =requests.get(url)
-
-        #Check if the request was successful (status code 200)
+        response = requests.get(url)
         if response.status_code == 200:
             print(f"Successfully visited URL: {url}")
         else:
             print(f"Failed to visit URL: {url}")
 
-        #initialize the Chrome driver
+        # initialize the Chrome driver
         driver = webdriver.Chrome()
         driver.maximize_window()
 
         # head to product page
         driver.get(url)
 
-        time.sleep(10)
+        time.sleep(2)
 
-        
-        #Close the browser window
+        try:
+            device_subtitle_element = driver.find_element(By.CSS_SELECTOR, ".details__content-subtitle a")
+            device_subtitle = device_subtitle_element.text
+            print(device_subtitle)
+
+            # Save the extracted information to a CSV file
+            with open('Products_info.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                if not header_written:
+                    writer.writerow(['Device Subtitle'])
+                    header_written = True
+                writer.writerow([device_subtitle])
+        except NoSuchElementException: # type: ignore
+            print(f"Could not find device subtitle for {url}")
+
+        # Close the browser window
         driver.quit()
+
+print("Information scraped and saved to verizon_device_info.csv")
