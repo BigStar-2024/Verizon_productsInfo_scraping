c191bd7df29f840e0cba204e5648221eb9ce831b
watanabe <kawanoaiyuki@gmail.com>
2024-05-01 23:46:11 +0900

    device title scraped

--- getProductsInfo.py
+++ getProductsInfo.py
@@ -3,10 +3,24 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.common.exceptions import NoSuchElementException
+import os
+
+# Check if the 'Products_info.csv' file exists
+if os.path.exists('Products_info.csv'):
+    # If the file exists, delete its contents
+    with open('Products_info.csv', 'w', newline='', encoding='utf-8') as file:
+        # Open the file in write mode to truncate its contents
+        pass
+
+# Create the CSV file if it doesn't exist
+with open('Products_info.csv', 'a', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+
+    writer.writerow(['Device Subtitle', 'Device Title'])
 
 with open('product_ids.csv', 'r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    header_written = False
+    reader = csv.reader(file) 
     for row in reader:
         product_id = row[0]
         url = f"https://opendevelopment.verizonwireless.com/device-showcase/device/{product_id}"
@@ -31,15 +45,16 @@ with open('product_ids.csv', 'r', encoding='utf-8') as file:
             device_subtitle = device_subtitle_element.text
             print(device_subtitle)
 
+            device_title_element = driver.find_elements(By.CSS_SELECTOR, ".details__content-title")
+            device_title = device_title_element[1].text
+            print(device_title)
+
             # Save the extracted information to a CSV file
             with open('Products_info.csv', 'a', newline='') as file:
                 writer = csv.writer(file)
-                if not header_written:
-                    writer.writerow(['Device Subtitle'])
-                    header_written = True
-                writer.writerow([device_subtitle])
-        except NoSuchElementException: # type: ignore
-            print(f"Could not find device subtitle for {url}")
+                writer.writerow([device_subtitle, device_title])
+        except NoSuchElementException:
+            print(f"Could not find device subtitle or title for {url}")
 
         # Close the browser window
         driver.quit()
